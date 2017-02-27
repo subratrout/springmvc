416849382cc66ed5ea815705d0894e714f12f0cc
Subrat Rout <Subrat.Rout@us.usana.com>
2017-02-27 16:13:34 -0700

    Added Listing of products by adding productController, ProductService and view file etc

---
+++ src/main/java/com/subratrout/services/ProductService.java
@@ -0,0 +1,12 @@
+package com.subratrout.services;
+
+import com.subratrout.domain.Product;
+
+import java.util.List;
+
+/**
+ * Created by subratrout on 2/27/17.
+ */
+public interface ProductService {
+    List<Product> listAllProducts();
+}
